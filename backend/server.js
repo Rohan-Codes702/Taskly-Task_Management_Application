bd16859d281a1b6d63466bb608648900e19ec3c5
Rohan-Codes702 <rohanmane702@gmail.com>
2026-02-26 23:38:13 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -44,5 +44,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
